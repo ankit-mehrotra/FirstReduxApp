40b41f9d86ca27b46cbf0c529df5490ff6735187
Ankit Mehrotra <ankit_mehrotra85@yahoo.com>
2017-06-26 13:22:49 +0530

    My First Redux App

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title:'Javascript The good parts',page: 101},
+        {title:'Harry Potter',page:200},
+        {title:'The dark tower',page:888},
+        {title:'Redux/React',page:999}
+    ]
+}
